4424ffa4818b604f0212ea074fbabdba75d533be
Joseph Crump <josephcrump79@yahoo.com>
2025-05-18 10:07:44 -0500

    react-hooks-component-mounting-lab completed

--- src/Timer.js
+++ src/Timer.js
@@ -4,9 +4,15 @@ class Timer extends Component {
   state = {
     time: 0,
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
-  };
+  }
 
-  // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     const { time, color } = this.state;
@@ -15,26 +21,24 @@ class Timer extends Component {
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
         <aside className="mountText">Mounted</aside>
-        <small onClick={this.handleClose}>X</small>
+        <small onClick={this.handleClose}>x</small>
       </section>
     );
   }
 
-  //clock functions
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
     }));
-  };
+  }
 
   stopClock = () => {
     clearInterval(this.interval);
-  };
+  }
 
-  // for the 'x' button,
   handleClose = () => {
     this.props.removeTimer(this.props.id);
-  };
+  }
 }
 
 export default Timer;
